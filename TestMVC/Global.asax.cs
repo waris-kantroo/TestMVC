f66063ecb64ddfccbd05f249b725e479ed4be355
waris-kantroo <47562096+waris-kantroo@users.noreply.github.com>
2019-03-12 18:47:45 +0530

    jquery datatable

--- TestMVC/Global.asax.cs
+++ TestMVC/Global.asax.cs
@@ -6,12 +6,18 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace TestMVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            // var container = new UnityContainer();  // newly added
+            // UnityConfig.RegisterTypes(container);    // newly added
+
+            var contain = UnityConfig.Container;   // newly added
+            UnityConfig.RegisterTypes(contain);   // newluy added
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ TestMVC/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC
+{
+    public static class Test 
+    {
+
+         static Test()
+        {
+
+        }
+    }
+}
