5938244315cf255ab3d3985214fcb5a9ec22e561
waris-kantroo <47562096+waris-kantroo@users.noreply.github.com>
2019-04-04 17:21:32 +0530

    test access modifier

--- TestMVC/test.cs
+++ TestMVC/test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;  // using for dll import
 using System.Web;
 
 namespace TestMVC
@@ -39,5 +40,58 @@ namespace TestMVC
     {
         void Display();
     }
+
+    public class CheckAccessModifier
+    {
+        int i;                                         //No Access specifier means private
+        public int j;                                        // Public
+        protected int k;                             //Protected data
+        internal int m;                        // Internal means visible inside assembly
+        protected internal int n;                   //inside assembly as well as to derived classes outside assembly
+        static int x;                                 // This is also private
+        public static int y;                       //Static means shared across objects
+        [DllImport("MyDll.dll")]
+        public static extern int MyFoo();       //extern means declared in this assembly defined in some other assembly
+
+        public void CheckAccessModifierMethod()
+        {
+            CheckAccessModifier obj = new CheckAccessModifier();
+            obj.i = 10;   //Error can’t access private data through object.But here it is accessible.:)
+            obj.j = 10;
+            obj.k = 10;
+            obj.m = 10;
+            obj.n = 10;
+            //     obj.s =10;  //Errror Static data can be accessed by class names only
+            CheckAccessModifier.x = 10;
+            //   obj.y = 10; //Errror Static data can be accessed by class names only
+            CheckAccessModifier.y = 10;
+        }
+
+
+    }
+
+    public class CheckAccessModifier2 : CheckAccessModifier
+    {
+        public static void Main()
+        {
+            CheckAccessModifier2 obj2 = new CheckAccessModifier2();
+            obj2.CheckAccessModifierMethod();   // using inheritance
+
+               //Access specifiers comes into picture only when you create object of class outside the class
+               CheckAccessModifier obj = new CheckAccessModifier();
+            
+            //    obj.i =10; //Error can’t access private data through object.
+            obj.j = 10;
+            //      obj.k=10;     //Error can’t access protected data through object.
+            obj.m = 10;
+            obj.n = 10;
+            //     obj.s =10;  //Errror Static data can be accessed by class names only
+           // CheckAccessModifier.x = 10;  //Error can’t access private data outside class
+                                         //   obj.y = 10; //Errror Static data can be accessed by class names only
+            CheckAccessModifier.y = 10;
+
+        }
+    }
+
 }
     
